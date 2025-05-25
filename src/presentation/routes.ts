900d9e93f77a23af1d39b3b231c3bcd3487327b0
matiasezleal <matileal@yahoo.com>
2025-05-25 14:56:05 -0300

    mongodb config with mongoose

--- src/presentation/routes.ts
+++ src/presentation/routes.ts
@@ -1,23 +1,15 @@
 import { Router } from 'express';
 import { AuthRoutes } from './auth/auth.routes';
 
-
-
-
 export class AppRoutes {
 
-
   static get routes(): Router {
-
     const router = Router();
 
     router.use('/api/auth', AuthRoutes.routes);
-    
-
 
     return router;
   }
-
 
 }
 
